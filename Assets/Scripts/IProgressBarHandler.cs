d24bd8c13205fa0935c6a7316d41a3d4d61ae4d8
Stanislav Kozlov <llarean@ya.ru>
2024-05-21 20:10:33 +0300

    Different changes
     The animation of changing screens has been changed
     Updated graphics resources
     Optimization
     The button click area has been increased
     Different fixes

---
+++ Assets/Scripts/IProgressBarHandler.cs
@@ -0,0 +1,10 @@
+﻿using EventBusSystem;
+
+public interface IProgressBarHandler : IGlobalSubscriber
+{
+    void SetColor(WorkoutStatus workoutStatus);
+    void StartAnimation(int currentTime);
+    void PauseAnimation();
+    void ResetAnimation();
+    void ChangeMaximumDuration(int duration);
+}
--- Assets/Scripts/Timer/View.cs
+++ Assets/Scripts/Timer/View.cs
@@ -4,26 +4,30 @@ using UnityEngine;
 
 public class View : MonoBehaviour
 {
+    [SerializeField] private CanvasGroup _canvasGroup;
     [SerializeField] private bool _isRight;
 
-    private float _showDuration = .3f;
+    private float _showDuration = .1f;
     private float _hideDuration = .1f;
 
     private float _showedPosition = 0;
-    private float _hidedPosition = 1400;
+    private float _hidedPosition = 700;
 
     public event Action OnEnabled;
 
     public void Show()
     {
         gameObject.SetActive(true);
-        gameObject.transform.DOLocalMoveX(_showedPosition, _showDuration);
+        
+        _canvasGroup.DOFade(1, _showDuration);
+        gameObject.transform.DOLocalMoveX(_showedPosition, _showDuration).SetEase(Ease.InSine);
     }
 
     public void Hide()
     {
         float hidedPosition = _isRight == true ? _hidedPosition : -_hidedPosition;
 
+        _canvasGroup.DOFade(0, _hideDuration);
         gameObject.transform.DOLocalMoveX(hidedPosition, _hideDuration).SetEase(Ease.Linear).OnComplete(() =>
         {
             gameObject.SetActive(false);
